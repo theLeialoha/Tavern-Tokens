5145f6cf20f72f3a97bc9b5b89e1a2013801da33
Leialoha <github@leialoha.dev>
2024-05-08 01:55:02 -0500

    v1.0.1 - fixed modification error

--- src/main/java/com/traverse/taverntokens/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/traverse/taverntokens/mixin/ServerPlayerEntityMixin.java
@@ -1,12 +1,16 @@
 package com.traverse.taverntokens.mixin;
 
 import com.traverse.taverntokens.interfaces.PlayerEntityWithBagInventory;
+import com.traverse.taverntokens.wallet.WalletScreenHandler;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerListener;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 @Mixin(ServerPlayerEntity.class)
@@ -16,6 +20,21 @@ public class ServerPlayerEntityMixin extends PlayerEntityMixin {
     @Inject(at = @At("RETURN"), method = "copyFrom")
     public void onRespawn(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo ci) {
         this.walletInventory.copy(((PlayerEntityWithBagInventory) oldPlayer).getWalletInventory());
+    }
+
+    @Redirect(
+        // the method this function is called in
+        method = "onScreenHandlerOpened",
+        // target the invocation of System.out.println
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/screen/ScreenHandler;addListener(Lnet/minecraft/screen/ScreenHandlerListener;)V"
+        )
+    )
+    public void onScreenHandlerOpened(ScreenHandler screenHandler, ScreenHandlerListener screenHandlerListener) {
+        if (!(screenHandler instanceof WalletScreenHandler)) {
+            screenHandler.addListener(screenHandlerListener);
+        }
     }
 
 }
